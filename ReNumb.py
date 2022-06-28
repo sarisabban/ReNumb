c266b2d9535a49b44eb4c27c100a7f0e427ad125
Sari Sabban <sari.sabban@gmail.com>
2022-06-28 22:04:04 +0300

    Update ReNumb.py

--- ReNumb.py
+++ ReNumb.py
@@ -2,27 +2,74 @@
 
 import sys
 
-def Motif(filename):
-	''' Renumbers a PDB structure '''
-	''' Generates a new file with the prefix _renumbered '''
-	pdb = open(filename , 'r')
-	NewFile = open(filename.split('.')[0] + '_renumbered.pdb' , 'w')
-	count = 0
-	num = 0
-	AA2 = None
-	for line in pdb:
-		if not line.startswith('ATOM'):			#Ignore all lines that do not start with ATOM
-			continue
-		else:
-			count += 1				#Sequencially number atoms
-			AA1 = line[23:27]			#Sequencially number residues
-			if not AA1 == AA2:
-				num += 1			
-			final_line = line[:7] + '{:4d}'.format(count) + line[11:17] + line[17:21] + 'A' + '{:4d}'.format(num) + line[26:]	#Update each line of the motif to have its atoms and residues sequencially labeled, as well as being in chain A
-			AA2 = AA1
-			NewFile.write(final_line)			#Write to new file called motif.pdb
-	NewFile.close()
+def Motif(filename, choice='resid'):
+	'''
+	Renumbers the amino acids in a PDB structure, it generates a new file
+	with the prefix FILENAME_renumbered.pdb
+	'''
+	with open(filename , 'r') as f:
+		with open(f'renumbered_{filename}', 'w') as F:
+			count = 0
+			newresid = 0
+			newchain = '@'
+			oldresid = None
+			oldchain = None
+			for line in f:
+				if line.startswith('ATOM'):
+					count += 1
+					ATOM     =       line[ 1-1:4 ].strip()
+					SERIAL   =   int(line[ 7-1:11])
+					NAME     =       line[13-1:16].strip()
+					ALTLOC   =       line[17-1   ].strip()
+					AMINO    =       line[18-1:20].strip()
+					CHAIN    =       line[22-1   ].strip()
+					RESIDUE  =   int(line[23-1:26])
+					INSERT   =       line[27-1   ].strip()
+					X        = float(line[31-1:38])
+					Y        = float(line[39-1:46])
+					Z        = float(line[47-1:54])
+					OCCUPY   = float(line[55-1:60])
+					TEMP     = float(line[61-1:66])
+					SEGMENT  =       line[73-1:76].strip()
+					ELEMENT  =       line[77-1:78].strip()
+					if RESIDUE != oldresid:
+						newresid += 1
+						oldresid = RESIDUE
+					if CHAIN != oldchain:
+						newchain = chr(ord(newchain) + 1)
+						oldchain = CHAIN
+					if choice   == 'atom': SERIAL = count
+					elif choice == 'resid': RESIDUE = newresid
+					elif choice == 'chain': CHAIN = newchain
+					newline  = f'{ATOM}  {SERIAL:>5}  {NAME:<3}{ALTLOC:>1}'
+					newline += f'{AMINO:>3} {CHAIN}'
+					newline += f'{RESIDUE:>4}{INSERT:>1}   {X:>8}{Y:>8}{Z:>8}'
+					newline += f'{OCCUPY:>6}{TEMP:>6}      '
+					newline += f'{SEGMENT:<4}{ELEMENT:>2}'
+				elif line.startswith('TER'):
+					count += 1
+					TER     =       line[ 1-1:4 ].strip()
+					SERIAL   =   int(line[ 7-1:11])
+					NAME     =       line[13-1:16].strip()
+					ALTLOC   =       line[17-1   ].strip()
+					AMINO    =       line[18-1:20].strip()
+					CHAIN    =       line[22-1   ].strip()
+					RESIDUE  =   int(line[23-1:26])
+					INSERT   =       line[27-1   ].strip()
+					if RESIDUE != oldresid:
+						newresid += 1
+						oldresid = RESIDUE
+					if CHAIN != oldchain:
+						newchain = chr(ord(newchain) + 1)
+						oldchain = CHAIN
+					if choice   == 'atom': SERIAL = count
+					elif choice == 'resid': RESIDUE = newresid
+					elif choice == 'chain': CHAIN = newchain
+					newline  = f'{TER}   {SERIAL:>5}  {NAME:<3}{ALTLOC:>1}'
+					newline += f'{AMINO:>3} {CHAIN}'
+					newline += f'{RESIDUE:>4}{INSERT:>1}'
+				else:
+					newline = line
+				F.write(newline + '\n')
 
-
-Filename = sys.argv[1]
-Motif(Filename)
+Motif(sys.argv[1], choice=sys.argv[2])
